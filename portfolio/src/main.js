6563b101d919968d30651ee1a00c8e61998b393f
tyzlddy <tyzlddy@naver.com>
2020-12-01 16:56:16 +0900

    [+] Add vue-typer, vuetify node package module

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import VueTyperPlugin from 'vue-typer'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+Vue.use(VueTyperPlugin)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
