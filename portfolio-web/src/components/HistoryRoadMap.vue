9073e40637768321d6c196a3110d9d085ebce441
TaeSun94 <tyzlddy@naver.com>
2021-02-11 16:54:44 +0900

    [+] Add HistoryRoadMap.vue

---
+++ portfolio-web/src/components/HistoryRoadMap.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <canvas id="historyLine"></canvas>
+    </v-container>
+</template>
+<script>
+export default {
+    mounted(){
+        var canvas = document.getElementById("historyLine");
+        this.canvas = canvas.getContext('2d');
+    }
+}
+</script>
+
+<style scoped>
+
+#historyLine{
+    background: red;
+    width: 80px;
+    height: 60px;
+}
+</style>
---
+++ portfolio-web/src/views/HistoryExp.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container id="content">
+        <h1>History</h1>
+        <RoadMap />
+    </v-container>
+</template>
+
+<script>
+import RoadMap from '@/components/HistoryRoadMap.vue';
+export default {
+    name:"HistoryEx",
+    components: {
+        RoadMap
+    }
+}
+</script>
+
+<style scoped>
+#content {
+    flex: 1;
+    justify-content: center;
+}
+
+#branch{
+    justify-content: center;
+    align-content: center;
+}
+</style>
